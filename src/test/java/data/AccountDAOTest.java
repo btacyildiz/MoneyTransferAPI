ade34501208dd12ed71d272eeddf4d4491f7cf83
Berkay <berkaytacyildiz@gmail.com>
2019-04-30 21:13:41 +0300

    postman collection is updated

--- src/test/java/data/AccountDAOTest.java
+++ src/test/java/data/AccountDAOTest.java
@@ -5,51 +5,40 @@ import model.Currency;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+
 public class  AccountDAOTest{
     @Test
     public void concurrencyTest(){
 
-        final int TRY_COUNT = 1000;
+        final int TRY_COUNT = 50;
+        final int THREAD_COUNT = 50;
         Account a = new Account("1", Currency.GBP, 0);
         AccountDAO.getInstance().addAccount(a);
 
+        ArrayList<Thread> threads = new ArrayList<>();
+        for(int i=0; i<TRY_COUNT; i++){
+            threads.add(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int i= TRY_COUNT;
+                    while(--i>0){
+                        Account b = AccountDAO.getInstance().getAccount(a.getAccountID());
+                        b.setBalance(b.getBalance() + 1.0);
+                    }
 
-        Thread readThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int i= TRY_COUNT;
-                while(i>0){
-                    Account b = AccountDAO.getInstance().getAccount(a.getAccountID());
-                    b.setBalance(b.getBalance() + 1.0);
+                    i= TRY_COUNT;
+                    while(--i>0){
+                        Account b = AccountDAO.getInstance().getAccount(a.getAccountID());
+                        b.setBalance(b.getBalance() - 1.0);
+                    }
                 }
-
-                i= TRY_COUNT;
-                while(i>0){
-                    Account b = AccountDAO.getInstance().getAccount(a.getAccountID());
-                    b.setBalance(b.getBalance() - 1.0);
-                }
-            }
-        });
-        Thread writeThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int i= TRY_COUNT;
-                while(--i>0){
-                    Account b = AccountDAO.getInstance().getAccount(a.getAccountID());
-                    b.setBalance(b.getBalance() + 1.0);
-                }
-
-                i= TRY_COUNT;
-                while(--i>0){
-                    Account b = AccountDAO.getInstance().getAccount(a.getAccountID());
-                    b.setBalance(b.getBalance() - 1.0);
-                }
-            }
-        });
+            }));
+        }
 
         synchronized(this){
-            readThread.start();
-            writeThread.start();
+            for(int i=0; i<THREAD_COUNT; i++)
+                threads.get(i).start();
         }
 
         Assert.assertEquals(0.0, AccountDAO.getInstance().getAccount(a.getAccountID()).getBalance());
